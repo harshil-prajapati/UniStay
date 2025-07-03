80136fbd52b6235c6b6abc4fd72f8cc3bbd3757e
harshil-prajapati <mr.harshilprajapati@gmail.com>
2025-07-03 21:15:19 +0530

    Pages Add And Router Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import './styles/App.css';
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './styles/App.css'
+
+// Pages  import
+import Home from './pages/Home/Home'
+import Hostel from './pages/Hostel/Hostel'
+import Student from './pages/Student/Student'
+import Admin from './pages/Admin/Admin'
+import AboutUs from './pages/AboutUs/AboutUs'
+import ContactUs from './pages/ContactUs/ContactUs'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <div className="app">
-      <h1>Welcome to UniStay 🏨</h1>
-      <p>Your hostel booking system.</p>
-    </div>
-    <Footer />
-    </>
+    <Router>
+      <Navbar />
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hostel" element={<Hostel />} />
+          <Route path="/student" element={<Student />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   )
 }
 
